b39d7b2aa0f23388e1973a6478f744986f406bcb
NeokoWorld <clea.flatres@viacesi.fr>
2025-01-18 17:30:06 +0100

    responsiv ?

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Page d'accueil chargée !");
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('header nav ul');
+
+    menuToggle.addEventListener('click', () => {
+        if (navMenu.classList.contains('show')) {
+            navMenu.classList.remove('show');
+            navMenu.classList.add('hide');
+        } else {
+            navMenu.classList.remove('hide');
+            navMenu.classList.add('show');
+        }
+    });
+
+    // Optionnel : Fermer le menu si un lien est cliqué
+    navMenu.addEventListener('click', (event) => {
+        if (event.target.tagName === 'A') {
+            navMenu.classList.remove('show');
+            navMenu.classList.add('hide');
+        }
+    });
+});
